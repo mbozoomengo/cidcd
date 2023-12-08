f56c5156588ba30516da781b1d2f778a149cc3f8
william <williammengo79@gmail.com>
2023-12-08 23:32:41 +0100

    update 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ci-cd-introduction app is running!';
+  title = 'ci-cd-introduction';
 }
