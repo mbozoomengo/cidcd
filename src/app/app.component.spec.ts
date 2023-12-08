1c31ecf36cdbd4dc50520e460b8c485c3b0bfb22
william <williammengo79@gmail.com>
2023-12-08 23:36:45 +0100

    update 5

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,9 +21,9 @@ describe('AppComponent', () => {
  });
 
  it('should render title', () => {
-   const fixture = TestBed.createComponent(AppComponent);
-   fixture.detectChanges();
-   const compiled = fixture.nativeElement as HTMLElement;
-   expect(compiled.querySelector('.content span')?.textContent).toContain('ci-cd-introduction');
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement as HTMLElement;
+  expect(compiled.querySelector('h1')?.textContent).toContain('ci-cd-introduction');
  });
 });
