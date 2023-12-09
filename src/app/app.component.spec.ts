7dcf5134b64fcc2160894a725a9a7d17e597e323
william <williammengo79@gmail.com>
2023-12-09 04:28:03 +0100

    update 8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MBOZOO MENGO ARTHUR WILLIAM');
+    expect(compiled.querySelector('h1')?.textContent).toContain('MBOZOO MENGO ARTHUR WILLIAM');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MBOZOO MENGO ARTHUR WILLIAM';
+  title: string = 'MBOZOO MENGO ARTHUR WILLIAM';
 }
